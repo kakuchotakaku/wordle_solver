eb7b690c40876c0f30c7d05c31b07132845c5ef2
kakucho takaku <miracle-spitzex@hotmail.co.jp>
2022-02-01 19:37:04 +0900

    少しきれいに

--- wordle_solver.rb
+++ wordle_solver.rb
@@ -2,16 +2,16 @@
 
 class WordleSolver
   def initialize
-    @wordlist_all = File.readlines("./wordlist_all.txt", chomp: true)
-    @wordlist_hidden = File.readlines("./wordlist_hidden.txt", chomp: true)
-    @letters = ("a".."z").to_a
+    @wordlist_all = File.readlines('./wordlist_all.txt', chomp: true)
+    @wordlist_hidden = File.readlines('./wordlist_hidden.txt', chomp: true)
+    @letters = ('a'..'z').to_a
   end
 
   def solve
-    while !final_answer?
+    until final_answer?
       guessed_word = calc_max_weight_word
       p guessed_word
-      STDOUT.flush
+      $stdout.flush
       input = gets.chomp.downcase
 
       reject_words_with_input(guessed_word, input)
@@ -37,14 +37,14 @@ class WordleSolver
     actions = guessed_word.chars.zip(input.chars)
     actions.each_with_index do |(letter, action), index|
       case action
-      when "b" # black
+      when 'b' # black
         @letters.delete(letter)
         @wordlist_all.reject! { |word| word.include?(letter) }
         @wordlist_hidden.reject! { |word| word.include?(letter) }
-      when "g" # green
-        @wordlist_all.reject! { |word| word[index] != letter }
-        @wordlist_hidden.reject! { |word| word[index] != letter }
-      when "y" # yellow
+      when 'g' # green
+        @wordlist_all.select! { |word| word[index] != letter }
+        @wordlist_hidden.select! { |word| word[index] != letter }
+      when 'y' # yellow
         @wordlist_all.reject! { |word| !word.include?(letter) || word[index] == letter }
         @wordlist_hidden.reject! { |word| !word.include?(letter) || word[index] == letter }
       end
@@ -55,6 +55,7 @@ class WordleSolver
     number_of_answer_waitlist = 0
     @wordlist_all.each do |word|
       return false if number_of_answer_waitlist > 1
+
       number_of_answer_waitlist += 1 if @wordlist_hidden.include?(word)
     end
     true
